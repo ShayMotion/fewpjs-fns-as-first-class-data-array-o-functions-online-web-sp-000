db8b6de013bc783b6af431f6db10a928674ec9d6
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-04-23 17:23:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 function wakeDog(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
+  let string = `Wake ${dogName} the ${dogBreed}`;
 }
 
 function leashDog(dogName, dogBreed) {
-  console.log(`Leash ${dogName} the ${dogBreed}`);
+ let string = `Leash ${dogName} the ${dogBreed}`;
 }
 
 function walkToPark(dogName, dogBreed) {
-  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+ let string = `Walk to the park with ${dogName} the ${dogBreed}`;
 }
 
 function throwFrisbee(dogName, dogBreed) {
-  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+let string = `Throw the frisbee for ${dogName} the ${dogBreed}`;
 }
 
 function walkHome(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
+let string = `Wake ${dogName} the ${dogBreed}`;
 }
 
 function unleashDog(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
+let string = `Wake ${dogName} the ${dogBreed}`;
+return(string);
 }
